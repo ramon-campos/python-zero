cbfd79e4b800a598258114420979849ba8188cb1
Ramon Campos <ramonramalhoc@outlook.com>
2022-05-30 14:22:22 -0300

    Create comparador-numeros-iguais.py

---
+++ condicionais/comparadores-exercicios/comparador-numeros-iguais.py
@@ -0,0 +1,9 @@
+# Solicita dois números ao usuário e comparar se eles são iguais.
+
+# Solicitando os números ao usuário
+n1 = float(input('Digite o primeiro número: '))
+n2 = float(input('Digite o segundo número: '))
+
+# Imprimindo o resultado da comparação
+print('O primeiro e o segundo número são iguais?')
+print('Resposta: ', n1 == n2)
